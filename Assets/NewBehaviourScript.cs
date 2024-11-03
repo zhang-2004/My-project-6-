8d71de124e0f08f90d660da014149b63cbe7394b
zhang-2004 <163397898+zhang-2004@users.noreply.github.com>
2024-11-03 12:08:46 +0800

    omer

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   public void  LoadMyScene(string sceneName)
+   {
+      SceneManager.LoadScene(sceneName);
+   }
+
+    public void  LoadMyScene(int sceneNumber)
+   {
+      SceneManager.LoadScene(sceneNumber);
+   }
+}
